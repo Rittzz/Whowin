55787dd4f6462618441f7e9f51a08d74435519d1
ian <ianbgraves@gmail.com>
2013-09-08 14:16:01 -0700

    Fixed up the classpaths and text formatting

--- whowin/src/com/rittzz/android/whowin/BaseActivity.java
+++ whowin/src/com/rittzz/android/whowin/BaseActivity.java
@@ -1,12 +1,32 @@
 package com.rittzz.android.whowin;
 
 import android.content.Intent;
+import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.rittzz.android.whowin.util.ViewServer;
 
 public abstract class BaseActivity extends SherlockFragmentActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ViewServer.get(this).addWindow(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ViewServer.get(this).removeWindow(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ViewServer.get(this).setFocusedWindow(this);
+    }
 
     @Override
     public boolean onPrepareOptionsMenu(final Menu menu) {
